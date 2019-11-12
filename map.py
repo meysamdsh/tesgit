3b36252768965ea0035209b37ce3fccc35654a85
meysam <info@venusdsh.ir>
2019-11-12 10:56:24 +0330

    first samplemap.py

---
+++ map.py
@@ -0,0 +1,10 @@
+# count = 0
+# for numbers in range(1, 10):
+#     if numbers % 2 ==0:
+#         count += 1
+#         print(numbers)  
+# print(f'We Have {count} even number') 
+
+a = 'hello'
+b = r'ven\n usdsh'
+print(b)
